dfab9d9d2faf3c170092820fd31b85b30ab8eae9
sigmasoldi3r <pablobc.1995@gmail.com>
2022-08-16 13:02:39 +0200

    Added boilerplate styling and stress test.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,51 @@
-import { init, GameLoop, Sprite } from "kontra";
+import { init, GameLoop, Sprite } from 'kontra'
 
-const { canvas } = init();
+const Let = <T, R>(a: T, b: (t: T) => R) => b(a)
 
-let sprite = Sprite({
-  x: 100, // starting x,y position of the sprite
-  y: 80,
-  color: "red", // fill color of the sprite rectangle
-  width: 20, // width and height of the sprite rectangle
-  height: 40,
-  dx: 2, // move the sprite 2px to the right every frame
-});
+Let(document.getElementById('root').style, _ => {
+  _.border = '1px solid black'
+  _.backgroundColor = 'gray'
+})
 
-let loop = GameLoop({
-  // create the main game loop
-  update: function () {
-    // update the game state
-    sprite.update();
+const { canvas } = init()
 
-    // wrap the sprites position when it reaches
-    // the edge of the screen
-    if (sprite.x > canvas.width) {
-      sprite.x = -sprite.width;
-    }
+const sprites = [
+  ...[...Array(20).keys()].map(k => ({
+    x: k * 41,
+    y: 80,
+    color: 'red',
+    width: 20,
+    height: 40,
+    dx: 2,
+  })),
+  ...[...Array(20).keys()].map(k => ({
+    x: k * 41,
+    y: 120,
+    color: 'blue',
+    width: 20,
+    height: 40,
+    dx: 4,
+  })),
+  ...[...Array(20).keys()].map(k => ({
+    x: k * 41,
+    y: 160,
+    color: 'green',
+    width: 20,
+    height: 40,
+    dx: 8,
+  })),
+].map(Sprite)
+
+GameLoop({
+  update() {
+    sprites.forEach(s => {
+      if (s.x > canvas.width) {
+        s.x = -s.width
+      }
+      s.update()
+    })
   },
-  render: function () {
-    // render the game state
-    sprite.render();
+  render() {
+    sprites.forEach(s => s.render())
   },
-});
-
-loop.start(); // start the game
+}).start()
