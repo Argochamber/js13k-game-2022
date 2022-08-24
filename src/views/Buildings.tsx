e2c52c4b799ef57a777a9c1810948cabc351d1fe
sigmasoldi3r <pablobc.1995@gmail.com>
2022-08-24 14:29:21 +0200

    Tint example

--- src/views/Buildings.tsx
+++ src/views/Buildings.tsx
@@ -1,5 +1,6 @@
 import { Game } from '../Game'
 import { rint } from '../lib'
+import { simplex } from '../noise'
 import { SpriteBuilder } from '../sprites'
 import { h, state } from '../ui'
 
@@ -21,12 +22,8 @@ SpriteBuilder.load([256, 256], 'sprites.png')
       ctx.imageSmoothingEnabled = false
       ctx.drawImage(builder.images[0]!, 0, 0, 256, 256)
       ctx.globalCompositeOperation = 'multiply'
-      for (let i = 0; i < 256; i++) {
-        for (let j = 0; j < 256; j++) {
-          ctx.fillStyle = `rgb(${rint(0, j)}, ${rint(0, i)}, ${rint(0, 255)})`
-          ctx.fillRect(i, j, 1, 1)
-        }
-      }
+      ctx.fillStyle = 'red'
+      ctx.fillRect(0, 0, builder.size[0], builder.size[1])
       ctx.globalCompositeOperation = 'destination-in'
       ctx.drawImage(builder.images[0]!, 0, 0, 256, 256)
     })
