00e1a761f5fd3a34ba5ffe90a52550467a910a36
sigmasoldi3r <pablobc.1995@gmail.com>
2022-08-24 10:04:33 +0200

    Added buildings.

---
+++ src/views/Buildings.tsx
@@ -0,0 +1,23 @@
+import { Game } from '../Game'
+import { h } from '../ui'
+
+type Props = { game: Game }
+
+export const BUILDINGS = [
+  { id: 'headquarter', title: 'Head Quarters' },
+  { id: 'resurrect-altar', title: 'Resurrection Altar' },
+  { id: 'soul-gate', title: 'Soul Gate' },
+  { id: 'ritual-mound', title: 'Ritual Mound' },
+  { id: 'tartarus-gate', title: 'Tartarus Gate' },
+] as const
+
+export const Buildings = ({ game }: Props) => (
+  <div>
+    {BUILDINGS.map(building => (
+      <div>
+        <div>{building.title}</div>
+        <hr />
+      </div>
+    ))}
+  </div>
+)
