4734adcc84cfa913a3fc1e31a3bc7ba6785922d9
sigmasoldi3r <pablobc.1995@gmail.com>
2022-08-25 11:09:03 +0200

    Improved sprite building.

--- src/sprites.ts
+++ src/sprites.ts
@@ -6,7 +6,10 @@
  * Procedural sprite builder, generates images.
  */
 export class SpriteBuilder {
-  constructor(readonly canvas: HTMLCanvasElement) {}
+  readonly context: CanvasRenderingContext2D
+  constructor(readonly canvas: HTMLCanvasElement) {
+    this.context = canvas.getContext('2d')!
+  }
 
   /**
    * Sets canvas size.
@@ -17,9 +20,7 @@ export class SpriteBuilder {
   }
 
   /**
-   * Adds a new image to the sprite builder, if needed.
-   * @param src The source.
-   * @returns
+   * Creates the image so it can be loaded.
    */
   async image(src: string) {
     const node = new Image()
@@ -31,6 +32,18 @@ export class SpriteBuilder {
     return node
   }
 
+  async print(src: string) {
+    const image = await this.image(src)
+    const w = image.naturalWidth
+    const h = image.naturalHeight
+    this.size(w, h)
+    this.context.drawImage(image, 0, 0)
+    return image
+  }
+
+  get info() {
+    return this.context.getImageData(0, 0, this.width, this.height)
+  }
   get url() {
     return this.canvas.toDataURL()
   }
@@ -54,10 +67,70 @@ export const sprite = async <T = void>(
   ) => Promise<T>
 ) => {
   const canvas = document.createElement('canvas')
-  const ctx = canvas.getContext('2d')
-  if (ctx == null) {
-    throw new Error(`Could not get the context.`)
-  }
   const builder = new SpriteBuilder(canvas)
-  return await block(builder, ctx)
+  return await block(builder, builder.context)
 }
+
+// type Vec2 = [number, number]
+type Vec4 = [number, number, number, number]
+type Pal = {
+  base: [Vec4, Vec4, Vec4, Vec4]
+  accent: [Vec4, Vec4, Vec4]
+  glint: [Vec4]
+}
+
+const palette = {
+  ffffff: ['base', 0],
+  bfbfbf: ['base', 1],
+  '535353': ['base', 2],
+  '090909': ['base', 3],
+  ff0000: ['accent', 0],
+  '01ff00': ['accent', 1],
+  '0000ff': ['accent', 2],
+  ffff00: ['glint', 0],
+} as Record<string, ['base' | 'accent' | 'glint', 0 | 1 | 2 | 3]>
+
+export const rgb2hex = (...c: number[]) =>
+  c.map(_ => _.toString(16).padStart(2, '0')).join('')
+
+export const colored = (src: string, target: Pal) =>
+  sprite(async (b, c) => {
+    const image = await b.image(src)
+    const w = image.naturalWidth
+    const h = image.naturalHeight
+    b.size(w, h)
+    c.drawImage(image, 0, 0)
+    const info = c.getImageData(0, 0, w, h)
+    for (let i = 0; i < info.data.length; i += 4) {
+      const r = info.data[i]!
+      const g = info.data[i + 1]!
+      const b = info.data[i + 2]!
+      const color = palette[`${rgb2hex(r, g, b)}`]
+      if (color != null) {
+        const c = target[color[0]][color[1]]!
+        info.data[i] = c[0]
+        info.data[i + 1] = c[1]
+        info.data[i + 2] = c[2]
+        info.data[i + 3] = c[3]
+      }
+    }
+    c.putImageData(info, 0, 0)
+    return b.url
+  })
+
+export const noised = (spr: string, amount: number) =>
+  sprite(async (b, c) => {
+    await b.print(spr)
+    c.globalCompositeOperation = 'multiply'
+    const info = b.info
+    for (let i = 0; i < info.data.length; i += 4) {
+      const r = 1 - Math.random() * amount
+      const g = 1 - Math.random() * amount
+      const b = 1 - Math.random() * amount
+      info.data[i] *= r
+      info.data[i + 1] *= g
+      info.data[i + 2] *= b
+    }
+    c.putImageData(info, 0, 0)
+    return b.url
+  })
