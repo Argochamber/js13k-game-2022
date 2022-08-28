bc9f2e2e4da241ccb5a51c3d6ed12bcf9db1c5e4
sigmasoldi3r <pablobc.1995@gmail.com>
2022-08-29 00:37:40 +0200

    Added island rendering to dashboard.

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,23 +1,41 @@
 import { Game } from '../Game'
 import { Island } from '../Island'
-import { h } from '../ui'
+import { h, state } from '../ui'
+import { genIslandAvatar } from './Islands'
 
 type Props = { game: Game }
 
+let s = null as Island | null
+const avatar = state('')
+
 export const Dashboard = ({ game }: Props) => {
   const island = Island.getIsland(game.selected[0], game.selected[1])
+  if (s !== island) {
+    genIslandAvatar(island).then(s => {
+      if (avatar.value !== s.data) {
+        avatar.value = s.data
+      }
+    })
+  }
   return (
     <div>
       <div>
         <div>{island.name} overview</div>
-        <div>
-          <h4>Buildings</h4>
-          <div>Headquarters: {island.buildings.hq}</div>
-          <div>Resurrection Altar: {island.buildings.altar}</div>
-          <div>Soul Gate: {island.buildings.soulgate}</div>
-          <div>Ritual Mound: {island.buildings.ritual}</div>
-          <div>Tartarus Gate: {island.buildings.tartarus}</div>
-          <hr />
+        <div className="flex" style={{ paddingTop: '2rem' }}>
+          <img
+            className="sprite"
+            style={{ marginTop: '2rem' }}
+            src={avatar.value}
+          />
+          <div className="wide">
+            <h4>Buildings</h4>
+            <div>Headquarters: {island.buildings.hq}</div>
+            <div>Resurrection Altar: {island.buildings.altar}</div>
+            <div>Soul Gate: {island.buildings.soulgate}</div>
+            <div>Ritual Mound: {island.buildings.ritual}</div>
+            <div>Tartarus Gate: {island.buildings.tartarus}</div>
+            <hr />
+          </div>
         </div>
       </div>
     </div>
--- src/views/Islands.tsx
+++ src/views/Islands.tsx
@@ -8,26 +8,26 @@ type Props = { game: Game }
 
 const images: Record<number, Atom<string>> = {}
 
+export const genIslandAvatar = (island: Island) => {
+  const isl = (island.seed * 0xffffff) >> 0
+  return $`*soul 1 1 f r 90 d 0 0
+*soul 1 1 f d 0 64
+*soul 1 1 d 64 64
+*soul 1 1 r 270 d 64 0
+*bone 0 0 d 32 32`.then(s =>
+    s.tint(
+      `rgb(${isl & 0xff}, ${(isl & 0xff) >> 16}, ${(isl & 0xff0000) >> 64})`,
+      'color-dodge'
+    )
+  )
+}
+
 export const Islands = ({ game }: Props) => {
   const islands: Array<Island> = Island.getIslands(game.name)
   for (const island of islands) {
     if (images[island.seed] == null) {
       images[island.seed] = state('')
-      const isl = (island.seed * 0xffffff) >> 0
-      $`*soul 1 1 f r 90 d 0 0
-*soul 1 1 f d 0 64
-*soul 1 1 d 64 64
-*soul 1 1 r 270 d 64 0
-*bone 0 0 d 32 32`
-        .then(s =>
-          s.tint(
-            `rgb(${isl & 0xff}, ${(isl & 0xff) >> 16}, ${
-              (isl & 0xff0000) >> 64
-            })`,
-            'color-dodge'
-          )
-        )
-        .then(s => (images[island.seed]!.value = s.data))
+      genIslandAvatar(island).then(s => (images[island.seed]!.value = s.data))
     }
   }
   return (
