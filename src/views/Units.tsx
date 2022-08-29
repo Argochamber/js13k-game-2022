d1a7023b61299492d4d9c0fe9e03f2b82d031503
sigmasoldi3r <pablobc.1995@gmail.com>
2022-08-29 08:23:44 +0200

    Added images to units

--- src/views/Units.tsx
+++ src/views/Units.tsx
@@ -30,8 +30,27 @@ const UNITS = [
 ] as const
 
 {
-  $`*bone 1 2 d 64 0
+  $`*bone 1 1 r 180 d 26 64
+    *bone 1 1 r 180 f d 36 64
     *bone 1 2 d 32 0`.then(s => (UNITS[0].icon.value = s.data))
+  $`*flesh 1 1 d 64 64
+    *flesh 1 1 r 90 d 0 64
+    *flesh 1 1 r 180 d 0 0
+    *flesh 1 1 f r 180 d 64 0
+    *dark 1 2 d 32 0
+    *dark 0 2 r 180 d 8 64
+    *dark 0 2 r 180 f d 56 64
+    `.then(s => (UNITS[1].icon.value = s.data))
+  $`*soul 2 1 a 0.5 d 0 64
+    *soul 2 1 a 0.5 f d 64 64
+    *soul 2 1 a 0.5 f d 64 54
+    *soul 2 1 a 0.5 d 0 54
+    *soul 1 2 a 0.5 d 32 0
+    *soul 1 2 a 0.5 d 28 8
+    *soul 1 2 a 0.5 d 36 8
+    *bone 2 1 d 8 60
+    *bone 2 1 f d 58 60
+    *bone 1 2 d 32 8`.then(s => (UNITS[2].icon.value = s.data))
 }
 
 export const Units = ({ game }: Props) => {
@@ -40,16 +59,9 @@ export const Units = ({ game }: Props) => {
     <div>
       <h2>Units in {island.name}</h2>
       <hr />
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className="flex col">
         {UNITS.map(unit => (
-          <div
-            style={{
-              display: 'flex',
-              width: '100%',
-              borderBottom: '2px solid gray',
-              padding: '1rem',
-            }}
-          >
+          <div className="flex ruler wide padded">
             <img
               src={unit.icon.value ?? ''}
               style={{ width: '128px', height: '128px', marginRight: '1rem' }}
